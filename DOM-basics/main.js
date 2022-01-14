17dd1dbf94614f9c164589542906a3761ade3716
Nabendu Biswas <nabendu.biswas@gmail.com>
2022-01-14 21:44:21 +0530

    Final commit

---
+++ DOM-basics/main.js
@@ -0,0 +1,57 @@
+//Grab Single element
+// const form = document.getElementById('my-form');
+// console.log(form);
+
+// console.log(document.querySelector('h1'));
+// console.log(document.querySelector('.container'));
+
+//Grab multiple elements
+// const nodeItems = document.querySelectorAll('.item');
+// const colItems = document.getElementsByClassName('item');
+//console.log(document.getElementsByTagName('li'));
+
+// nodeItems.forEach(item => console.log(item));
+//colItems.forEach(item => console.log(item));
+// for(let i = 0; i < colItems.length; i++){
+//     console.log(colItems[i]);
+// }
+
+//DOM Manipulation
+
+
+// btn.style.backgroundColor = 'blue';
+// btn.style.color = 'white';
+
+//Event listener
+// const btn = document.querySelector('.btn');
+// const ul = document.querySelector('.items');
+
+// btn.addEventListener('click', e => {
+//     e.preventDefault();
+//     ul.firstElementChild.textContent = 'HTML';
+//     ul.children[1].innerText = 'CSS';
+//     ul.lastElementChild.innerHTML = '<h4>JavaScript</h4>';
+// })
+
+const myForm = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const msg = document.querySelector('.msg');
+const userList = document.querySelector('#users');
+
+myForm.addEventListener('submit', onSubmit);
+
+function onSubmit(e){
+    e.preventDefault();
+    if(nameInput.value === '' || emailInput.value === ''){
+        msg.classList.add('error');
+        msg.innerText = 'Please enter all fields';
+        setTimeout(() => msg.remove(), 3000);
+    } else {
+        const li = document.createElement('li');
+        li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value}`));
+        userList.appendChild(li);
+        nameInput.value = '';
+        emailInput.value = '';
+    }
+}
